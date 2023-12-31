8a5255875ad732e1ff374014564132f899d014b9
mounika2121 <chmounika2121@gmail.com>
2023-12-31 14:30:38 +0000

    Basic Navbar created

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import NavBar from './components/NavBar'
+
+import './App.css'
+
+const App = () => <NavBar />
+
+export default App
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,25 @@
+import './index.css'
+
+const Navbar = () => (
+  <div>
+    <img
+      src="https://res.cloudinary.com/djbs4yqbz/image/upload/v1704031223/Stack_Overflow-Logo.wine_ll2sa8.png"
+      alt="logo"
+      className="nav-logo"
+    />
+    <div>
+      <div>QUESTIONS</div>
+      <div>JOBS</div>
+      <div>DOCUMENTATION</div>
+      <div>TAGS</div>
+      <div>USERS</div>
+      <div>BADGES</div>
+      <div>ASK QUESTION</div>
+      <div>STACK EXCHANGE</div>
+      <div>QUESTIONS</div>
+      <div>INBOX</div>
+    </div>
+  </div>
+)
+
+export default Navbar
